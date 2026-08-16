a89bb50e33cf50b72e1488a703fba0f8fbde63e4
kratigithubbeohar <kratibeohar@gmail.com>
2026-08-16 15:35:24 +0530

    langgraph chatbot with memory

---
+++ apps/langgraph_chatbot.py
@@ -0,0 +1,65 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from pydantic import BaseModel
+from langchain_groq import ChatGroq
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import InMemorySaver
+from typing import Annotated
+
+
+class ChatState(BaseModel):
+    messages: Annotated[list, add_messages]
+
+
+llm = ChatGroq(
+    model="openai/gpt-oss-20b"
+)
+
+
+def ChatBotNode(state: ChatState):
+    res = llm.invoke(state.messages)
+
+    return {
+        "messages": [res]
+    }
+
+
+memory = InMemorySaver()
+
+graph = StateGraph(ChatState)
+
+graph.add_node("ChatBot", ChatBotNode)
+
+graph.add_edge(START, "ChatBot")
+graph.add_edge("ChatBot", END)
+
+graph = graph.compile(checkpointer=memory)
+
+
+while True:
+    query = input("User: ")
+
+    if query.lower() in ['quit', 'exit', 'bye']:
+        print("Thanks for chatting!")
+        break
+
+    res = graph.invoke(
+        {
+            "messages": [   # messages, not message
+                {
+                    "role": "user",
+                    "content": query
+                }
+            ]
+        },
+        {
+            "configurable": {
+                "thread_id": "my-bot-1"
+            }
+        }
+    )
+
+    ans = res["messages"][-1].content
+    print("AI:", ans)
